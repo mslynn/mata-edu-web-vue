abe9a8f03438149329535eea898e0e91b2cd8452
zlm <1104567294@qq.com>
2025-12-08 18:32:29 +0800

    nuxt配置文件

--- .nuxt/types/layouts.d.ts
+++ .nuxt/types/layouts.d.ts
@@ -1,5 +1,5 @@
 import type { ComputedRef, MaybeRef } from 'vue'
-export type LayoutKey = "auth" | "default"
+export type LayoutKey = "auth" | "default" | "teacher"
 declare module 'nuxt/app' {
   interface PageMeta {
     layout?: MaybeRef<LayoutKey | false> | ComputedRef<LayoutKey | false>
